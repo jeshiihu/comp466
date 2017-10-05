fb2c33b8bca77afcc5f64e302edb1aa91a74de3f
jeshiihu <jeshiihu@gmail.com>
2017-10-04 20:42:42 -0700

    slideshow almost done just needs transitions

--- TMA1/part3/slideshow.js
+++ TMA1/part3/slideshow.js
@@ -15,6 +15,10 @@ var photos = {
 				"Victoria Island View from the waters", "Richmond World Fest - Dragonette"]
 };
 
+var shuffle = false;
+var play = false;
+var shuffledList = new Array();
+
 var slideshow = document.getElementById('slideshow');
 var context = slideshow.getContext('2d');
 window.onload = function()
@@ -30,6 +34,8 @@ function GetImage()
     	DisplayImage(im);
     };
 	im.src = photos.img[index];
+	var photoIndex = document.getElementById("photoIndex");
+	photoIndex.textContent = "Photo " + (index+1);
 }
 
 function DisplayImage(im)
@@ -46,9 +52,89 @@ function DisplayImage(im)
     context.fillText(photos.caption[index], slideshow.width/2, slideshow.height-20);
 }
 
+var cbxSize = document.getElementById("size");
+cbxSize.onchange = function()
+{
+	if(cbxSize.value == "l")
+	{
+		slideshow.width = "1024";
+		slideshow.height= "768";
+	}
+	else if(cbxSize.value == "m")
+	{
+		slideshow.width = "640";
+		slideshow.height= "480";
+	}
+	else
+	{
+		slideshow.width = "320";
+		slideshow.height= "240";
+	}
+
+	GetImage();
+}
+
+var shuffleBtn = document.getElementById("shuffle");
+shuffleBtn.onclick = function() 
+{
+	if(shuffleBtn.getAttribute("src") == "Icons/shuffle_off.png")
+	{
+		shuffleBtn.setAttribute("src", "Icons/shuffle_on.png");
+		shuffle = true;
+		shuffledList.push(index);
+	}	
+	else
+	{
+		shuffleBtn.setAttribute("src", "Icons/shuffle_off.png");
+		shuffle = false;
+	}	
+}
+
+var playBtn = document.getElementById("playStop");
+playBtn.onclick = function() 
+{
+	if(playBtn.getAttribute("src") == "Icons/play.png")
+	{
+		playBtn.setAttribute("src", "Icons/stop.png");
+		play = true;
+		setTimeout(StartShow, 2000);
+	}
+	else
+	{
+		playBtn.setAttribute("src", "Icons/play.png");
+		play = false;
+	}
+}
+
+function StartShow()
+{
+	if(!play) return;
+	if(!shuffle)
+		index++
+	else
+		index = GetRandom();
+	GetImage();
+	setTimeout(StartShow, 2000);
+}
+
+function GetRandom()
+{
+	if(shuffledList.length == photos.img.length)
+		shuffledList = new Array();
+
+	var newIndex = Math.floor(Math.random() * (photos.img.length-1));
+	if(shuffledList.indexOf(newIndex) > -1)
+		return GetRandom();
+
+	shuffledList.push(newIndex);
+	return newIndex;
+}
+
 var prevArrow = document.getElementById('prevArrow');
 prevArrow.onclick = function() 
 {
+	if(shuffle) return;
+
 	if(index == 0)
 		index = 19;
 	else
@@ -59,6 +145,8 @@ prevArrow.onclick = function()
 var nextArrow = document.getElementById('nextArrow');
 nextArrow.onclick = function() 
 {
+	if(shuffle) return;
+
 	if(index == 19)
 		index = 0;
 	else
