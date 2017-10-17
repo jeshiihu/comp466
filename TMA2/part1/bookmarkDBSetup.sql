6bc113ad85acb0f3f5227c1d0f906c1a7b580678
jeshiihu <jeshiihu@gmail.com>
2017-10-16 23:35:35 -0700

    script for setup DB

---
+++ TMA2/part1/bookmarkDBSetup.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS BookmarksDB;
+CREATE DATABASE BookmarksDB;
+USE BookmarksDB;
+
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users
+(
+	UserName varchar(225) NOT NULL PRIMARY KEY,
+	Password varchar(225) NOT NULL
+);
+
+DROP TABLE IF EXISTS Bookmarks;
+CREATE TABLE Bookmarks
+(
+	BookmarkID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	Url varchar(225) NOT NULL,
+	User varchar(225) NOT NULL,
+	FOREIGN KEY (User) REFERENCES Users(UserName)
+);
